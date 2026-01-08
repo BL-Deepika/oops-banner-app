cc1949d209d672d0c42a7e52804fa37a7a667dce
BL-Deepika <deepika.ramireddy@bridgelabz.com>
2026-01-08 22:33:34 +0530

    OOPs as Banner using space and *

--- src/com/banner/oops/OOPSBannerApp.java
+++ src/com/banner/oops/OOPSBannerApp.java
@@ -3,6 +3,30 @@ package com.banner.oops;
 public class OOPSBannerApp{
 
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        String[] dataText = {
+                String.join("  ",
+                        "   ***  ", "    ***  ", " ****** ", "     *****"),
+                String.join("  ",
+                        " **   **", "  **   **", " **    **", "  **    "),
+                String.join("  ",
+                        "**     **", "**     **", "** ", "  **", "**"),
+                String.join("  ",
+                        "**     **", "**     **", "**    **", "   **   "),
+                String.join("  ",
+                        "**     **", "**     **", "******    ", "   ***"),
+                String.join("  ",
+                        "**     **", "**     **", "**    ", "          **"),
+                String.join("  ",
+                        "**     **", "**     **", "**    ", "           **"),
+                String.join("  ",
+                        " **   **", "  **   **", " **    ", "          **"),
+                String.join("  ",
+                        "   ***   ", "   ***   ", "**    ", "     *****")
+        };
+
+        for (String line : dataText) {
+            System.out.println(line);
+        }
     }
 }
